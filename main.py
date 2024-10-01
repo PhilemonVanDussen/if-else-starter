ec6e91b6288a3fccea280659a259cb43b42157a8
PhilemonVanDussen <vandussenph709@northwested.org>
2024-10-01 14:29:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+# PJ VanDussen
+# 10/1/2024
+# If Else Statment
 
+#Task 2
+# alien_color = 'purple'
+# if alien_color == 'green':
+#     print('You score 5 points!')
+# else:
+#     print('You score 10 points!!')
+
+# #Task 3
+# f_name = input('Please enter your first name')
+# # if f_name.len(5):
+# if len(f_name) <= 5:
+#     print('Your first name is too short')
+# else:
+#     print('Your name is long')
+
+# #Task 4
+vowels = ['a', 'e', 'i', 'o', 'u']
+letter = input('Enter a letter in the alphabet ').lower()
+if letter in vowels:
+    print('Your letter is a vowel')
+else:
+    print('Your letter is a constant')
+#Task 5
+# num1 = int(input('Please enter a number'))
+# num2 = int(input('Enter another number'))
+# num1 % num2
